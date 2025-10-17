e5ec151d6223c4e2a651903ce93f2ace1313e43a
sachindhee <154836258+sachindhee@users.noreply.github.com>
2025-10-18 00:09:17 +0530

    init razoroayclient

--- src/main/java/com/razorpay/Service/OrderService.java
+++ src/main/java/com/razorpay/Service/OrderService.java
@@ -1,10 +1,16 @@
 package com.razorpay.Service;
 
+import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import com.razorpay.model.Orders;
 import com.razorpay.repository.OrderRepository;
+import jakarta.annotation.PostConstruct;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class OrderService {
@@ -19,7 +25,25 @@ public class OrderService {
 
     private RazorpayClient razorpayClient;
 
+    @PostConstruct
+    public void init() throws RazorpayException {
+        this.razorpayClient = new RazorpayClient(razorpayId,razorpaySeceret);
+    }
 
+    public Order createOrder(Orders order) throws RazorpayException {
+
+        JSONObject json = new JSONObject();
+        json.put("amount",order.getAmount());
+        json.put("currency","INR");
+        json.put("recept",order.getEmail());
+
+        Order order1 = razorpayClient.orders.create(json);
+        order.setRazorpayOrderId(order1.get("id"));
+        order.setOrderStatus(order1.get("status"));
+
+       return orderRepository.save(order1);
+
+    }
 
 
 
